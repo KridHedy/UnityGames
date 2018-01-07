307854d0b76410e7300580d367944c3e8adbc9a1
Saad Achraf <drygincoding@hotmail.com>
2018-01-07 12:40:41 +0000

    Character Contoller and Commented camera code
    character

---
+++ GGJ2018/Assets/Scripts/CameraController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+/*
+    public Transform target;
+    public Vector3 offsetPos;
+    public float moveSpeed = 5;
+    public float turnSpeed = 10;
+    public float smoothSpeed = 0.5f;
+
+    Quaternion targetRotation;
+    Vector3 targetPos;
+    bool smoothRotating = false;
+
+    private void Update()
+    {
+        MoveWithTarget();
+        LookAtTarget();
+
+        if (Input.GetKeyDown(KeyCode.G) && !smoothRotating)
+        {
+            StartCoroutine("RotateAroundTarget", 45);
+        }
+
+        if (Input.GetKeyDown(KeyCode.H) && !smoothRotating)
+        {
+            StartCoroutine("RotateAroundTarget", -45);
+        }
+
+
+    }
+
+    /// <summary>
+    /// Move the camerat to the player position + current camera offset
+    /// Offset is modified by the RotateRoundTarget coroutine
+    /// </summary>
+    void MoveWithTarget() {
+        targetPos = target.position + offsetPos;
+        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Use the look vector (target - current) to aim the camera toward the player
+    /// </summary>
+    void LookAtTarget() {
+        targetRotation = Quaternion.LookRotation(target.position - transform.position);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// This coroutine can only have one instance running at a time
+    /// Determined by 'smoothRotating'
+    /// </summary>
+    IEnumerator RotateAroundTarget(float angle) {
+        Vector3 vel = Vector3.zero;
+        Vector3 targetOffsetPos = Quaternion.Euler(0, angle, 0) * offsetPos;
+        float dist = Vector3.Distance(offsetPos, targetOffsetPos);
+        smoothRotating = true;
+
+        while (dist > 0.02f) {
+            offsetPos = Vector3.SmoothDamp(offsetPos, targetOffsetPos, ref vel, smoothSpeed);
+            dist = Vector3.Distance(offsetPos, targetOffsetPos);
+            yield return null;
+        }
+
+        smoothRotating = false;
+        offsetPos = targetOffsetPos;
+    }
+*/
+}
---
+++ GGJ2018/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float velocity = 5f;
+    public float turnSpeed = 12f;
+
+    Vector2 input;
+    float angle;
+
+    Quaternion targetRotation;
+    Transform cam;
+
+    private void Start()
+    {
+        cam = Camera.main.transform;
+    }
+
+    private void Update()
+    {
+        GetInput();
+
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) return;
+
+        CalculateDirection();
+        Rotate();
+        Move();
+    }
+
+    /// <summary>
+    /// Input based on Horizontal(a,d,<,>) and Vertical (w,s,^,v) keys
+    /// </summary>
+    void GetInput() {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+    }
+
+    /// <summary>
+    /// Direction relative to the camera's rotation
+    /// </summary>
+    void CalculateDirection() {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        angle += cam.eulerAngles.y;
+    }
+
+    /// <summary>
+    /// Rotate toward the calculated angle
+    /// </summary>
+    void Rotate() {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// This player only moves along its own forward axis
+    /// </summary>
+    void Move() {
+        transform.position += transform.forward * velocity * Time.deltaTime;
+    }
+}
