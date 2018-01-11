caf564b20b57034dddf7972a712a8068439ad283
Saad Achraf <drygincoding@hotmail.com>
2018-01-11 13:36:11 +0000

    d
    d

--- GGJ2018/Assets/Scripts/CameraController.cs
+++ GGJ2018/Assets/Scripts/CameraController.cs
@@ -1,72 +1,119 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
+using System.Collections;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
     public Transform target;
-    public Vector3 offsetPos;
-    public float moveSpeed = 5;
-    public float turnSpeed = 10;
-    public float smoothSpeed = 0.5f;
 
-    Quaternion targetRotation;
-    Vector3 targetPos;
-    bool smoothRotating = false;
+    public float distance = 5.0f;
+
+    public float maxDistance = 20;
+    public float minDistance = .6f;
+
+    public float xSpeed = 250.0f;
+    public float ySpeed = 120.0f;
+
+    public int yMinLimit = -80;
+    public int yMaxLimit = 80;
+
+    public int zoomRate = 40;
+
+    public float rotationDampening = 3.0f;
+    public float zoomDampening = 5.0f;
+
+    private float targetHeight = 1.7f;
+    private float x = 0.0f;
+    private float y = 0.0f;
+    private float currentDistance;
+    private float desiredDistance;
+    private float correctedDistance;
+
+    void Start()
+    {
+        Vector3 angles = transform.eulerAngles;
+        x = angles.x;
+        y = angles.y;
+
+        currentDistance = distance;
+        desiredDistance = distance;
+        correctedDistance = distance;
+
+        targetHeight = target.localScale.y;
+
+        // Make the rigid body not change rotation
+        if (GetComponent<Rigidbody>())
+            GetComponent<Rigidbody>().freezeRotation = true;
+    }
 
     private void Update()
     {
-        MoveWithTarget();
-        LookAtTarget();
 
-        if (Input.GetKeyDown(KeyCode.G) && !smoothRotating)
+        // Don't do anything if target is not defined
+        if (!target)
+            return;
+
+        // The mouse govern camera position
+        x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+        y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+        
+        // Zooming
+        desiredDistance -= Input.GetAxis("Mouse ScrollWheel") * Time.deltaTime * zoomRate * Mathf.Abs(desiredDistance);
+
+    }
+
+    /**
+     * Camera logic on LateUpdate to only update after all character movement logic has been handled.
+     */
+    private void LateUpdate()
+    {
+        y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+        // Set camera rotation
+        Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+        // Calculate the desired distance
+        desiredDistance = Mathf.Clamp(desiredDistance, minDistance, maxDistance);
+        correctedDistance = desiredDistance;
+
+        // Calculate desired camera position
+        Vector3 position = target.position - (rotation * Vector3.forward * desiredDistance + new Vector3(0, targetHeight, 0));
+
+        // Check for collision using the true target's desired registration point as set by user using height
+        RaycastHit collisionHit;
+        Vector3 trueTargetPosition = new Vector3(target.position.x, target.position.y + targetHeight, target.position.z);
+
+        // If there was a collision, correct the camera position and calculate the corrected distance
+        bool isCorrected = false;
+
+        if (Physics.Linecast(trueTargetPosition, position, out collisionHit))
         {
-            StartCoroutine("RotateAroundTarget", 45);
+            if (Vector3.Distance(trueTargetPosition, transform.position) < Vector3.Distance(trueTargetPosition, position))
+                position = collisionHit.point;
+            correctedDistance = Vector3.Distance(trueTargetPosition, position);
+            isCorrected = true;
         }
 
-        if (Input.GetKeyDown(KeyCode.H) && !smoothRotating)
-        {
-            StartCoroutine("RotateAroundTarget", -45);
-        }
+        // For smoothing, lerp distance only if either distance wasn't corrected, or correctedDistance is more than currentDistance
+        if (!isCorrected || correctedDistance >= currentDistance)
+                desiredDistance = Mathf.Lerp(desiredDistance, correctedDistance, Time.deltaTime * zoomDampening);
+            else
+                desiredDistance = correctedDistance;
 
+        // Recalculate position based on the new currentDistance
+        position = target.position - (rotation * Vector3.forward * desiredDistance + new Vector3(0, -targetHeight / 1.5f, 0));
 
+        transform.rotation = rotation;
+        transform.position = position;
     }
 
-    /// <summary>
-    /// Move the camerat to the player position + current camera offset
-    /// Offset is modified by the RotateRoundTarget coroutine
-    /// </summary>
-    void MoveWithTarget() {
-        targetPos = target.position + offsetPos;
-        transform.position = Vector3.Lerp(transform.position, targetPos, moveSpeed * Time.deltaTime);
+    private static float ClampAngle(float angle, float min, float max)
+    {
+        if (angle < -360)
+            angle += 360;
+        if (angle > 360)
+            angle -= 360;
+        return Mathf.Clamp(angle, min, max);
     }
-
-    /// <summary>
-    /// Use the look vector (target - current) to aim the camera toward the player
-    /// </summary>
-    void LookAtTarget() {
-        targetRotation = Quaternion.LookRotation(target.position - transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
-    }
-
-    /// <summary>
-    /// This coroutine can only have one instance running at a time
-    /// Determined by 'smoothRotating'
-    /// </summary>
-    IEnumerator RotateAroundTarget(float angle) {
-        Vector3 vel = Vector3.zero;
-        Vector3 targetOffsetPos = Quaternion.Euler(0, angle, 0) * offsetPos;
-        float dist = Vector3.Distance(offsetPos, targetOffsetPos);
-        smoothRotating = true;
-
-        while (dist > 0.02f) {
-            offsetPos = Vector3.SmoothDamp(offsetPos, targetOffsetPos, ref vel, smoothSpeed);
-            dist = Vector3.Distance(offsetPos, targetOffsetPos);
-            yield return null;
-        }
-
-        smoothRotating = false;
-        offsetPos = targetOffsetPos;
-    }
-
 }
