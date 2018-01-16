2b75cf84ce59db1efb652e594ab672736a6d63e9
Hedy Krid <hedireaper@gmail.com>
2018-01-16 20:36:18 +0100

    com
    com

---
+++ Valley Voxel/Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToMove : MonoBehaviour {
+
+    Vector3 targetPostion;
+    Vector3 lookAtTarget;
+    Quaternion playerRot;
+
+    float rotSpeed = 7f;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButton(0))
+        {
+            SetTargetPosition();
+        }
+	}
+
+    void SetTargetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray,out hit ,1000))
+        {
+            targetPostion = hit.point;
+            // this.transform.LookAt(targetPostion);
+            lookAtTarget = new Vector3(targetPostion.x - transform.position.x, transform.position.y, targetPostion.z - transform.position.z);
+            playerRot = Quaternion.LookRotation(lookAtTarget);
+        }
+    }
+
+    void Move()
+    {
+        transform.rotation = Quaternion.Slerp(transform.rotation, playerRot, rotSpeed * Time.deltaTime);
+    }
+}
